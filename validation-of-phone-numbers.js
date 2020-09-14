658353497989f879f6c6c70994e2281876a1d6c8
nolwazi749 <nolwazizwane7@gmail.com>
2020-09-14 05:25:30 -0400

    i changed my variable , because it was giving me syntax error

--- validation-of-phone-numbers.js
+++ validation-of-phone-numbers.js
@@ -1,5 +1,6 @@
 function phoneNumberCheck(str) {
-    var myRegex = /^(1\s?)?(\(\d{3}\)/gi;
-    return myRegex.test()
-}
-console.log(phoneNumberCheck(0854317829));
+    var myRegex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/gi;
+    return myRegex.test(str);
+  }
+  
+console.log(phoneNumberCheck("0754673489"))

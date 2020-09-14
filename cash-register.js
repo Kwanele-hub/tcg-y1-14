480fa74d09e9abe25726d65d9cf77440e657b142
nolwazi749 <nolwazizwane7@gmail.com>
2020-09-14 07:50:55 -0400

    cash register code

--- cash-register.js
+++ cash-register.js
@@ -1,3 +1,14 @@
-function checkCashRgister(num){
-    
-}
+function checkCashRegister(cart){
+        var items = Object.keys(cart); 
+        var sum = 0; 
+        for (var i = 0; i < items.length; i++) {
+          var itemName = items[i];
+          var itemPrice = cart[itemName]; 
+          sum += Number.parseFloat(itemPrice); 
+        }
+        if (cart==items){
+            return {status: "INSUFFICIENT_FUNDS", change: []}
+        }
+        return sum;
+      };
+console.log(checkCashRegister(56 + 78));

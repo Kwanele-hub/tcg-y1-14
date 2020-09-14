18d16301f236c5dca528b40e7af1273bf7be7e22
nolwazi749 <nolwazizwane7@gmail.com>
2020-09-14 03:51:37 -0400

    this the if statement line which will help me with the result when i console my code

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,8 @@
 function isPalindrome(str){
 str = str.toLowerCase().replace(/[\W_]/gi);
 var reversedStr = str.split("").reverse().join("");
+if (str==reversedStr){
+    return true
+}
+return false
+};
